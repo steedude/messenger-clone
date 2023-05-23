c5b5517664e3d7ff19d54342f3299a5faf9b4709
steedude <85280987+steedude@users.noreply.github.com>
2023-05-23 12:24:10 +0800

    加入prettier

---
+++ app/(site)/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Logo from '@/public/images/logo.png';
+
+export default function Home() {
+  return (
+    <div className='flex min-h-full flex-col justify-center bg-gray-100 py-12 sm:px-6 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <Image
+          alt='logo'
+          height={48}
+          width={48}
+          className='mx-auto w-auto'
+          src={Logo}
+        />
+        <h2
+          className='
+            mt-6 
+            text-center 
+            text-3xl 
+            font-bold 
+            tracking-tight 
+            text-gray-900
+          '
+        >
+          Sign in to your account
+        </h2>
+      </div>
+      {/* AuthForm */}
+    </div>
+  );
+}
